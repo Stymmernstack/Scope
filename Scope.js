20d911c77f091feb78fad331e91e824bc1e6f552
stym mernstack <satyamsoni5202@gmail.com>
2025-02-02 10:42:03 +0530

    add the scope file

---
+++ Scope.js
@@ -0,0 +1,15 @@
+// function outer()
+// {
+//     let name = 'stym' ;
+// }
+
+
+// var age = [1,5,6,8]
+// console.log(age);
+// name = age.find(age=>age % 2 == 0)
+// console.log(name);
+
+var age = [1,5,6,8]
+console.log(age);
+name = age.findLast(age=>age % 2 == 0)
+console.log(name);
